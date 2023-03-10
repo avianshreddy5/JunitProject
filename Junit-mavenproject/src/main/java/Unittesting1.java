1ef1c39dd5df836b2a038caba7410ad93eb65550
admin <admin@DESKTOP-SFBVSET>
2023-03-10 22:06:37 +0530

    first commitee

---
+++ Junit-mavenproject/src/main/java/Unittesting1.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class Unittesting1 {
+
+	// TODO Auto-generated method stub
+	@Test
+	public void add() {
+		System.out.println("add" (10 + 20));
+
+	}
+
+}
